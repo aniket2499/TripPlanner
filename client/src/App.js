1e133a95862614c9a6cca364f6c0cf211851619b
Mav-ninerniner <mpate138@stevens.edu>
2022-11-29 18:19:09 -0500

    Added login and authorization

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,33 @@
+import React from "react";
 import "./App.css";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from './components/Home';
+import Account from './components/Account';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import Navigation from './components/Navigation';
+import {AuthProvider} from './firebase/Auth';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
-  return <div className="App">HELLO</div>;
+  console.log("App.js");
+  return(
+    <AuthProvider> 
+      <Router>
+        <Navigation />
+        <Routes>
+        <Route path='/' element={<PrivateRoute />}>
+          <Route exact path="/" element={<Home/>} />
+        </Route>
+        <Route path='/account' element={<PrivateRoute />}>
+          <Route path="/account" element={<Account/>} />
+        </Route>
+          <Route path="/login" element={<Login/>} />
+          <Route path="/signup" element={<SignUp/>} />
+        </Routes>
+      </Router>  
+    </AuthProvider>
+    );
 }
 
 export default App;
---
+++ client/src/components/Login.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "../App.css";
+
+function Login() {
+  return <div className="App">Login</div>;
+}
+
+export default Login;
