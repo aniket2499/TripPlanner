d9548714b4e9b9fedc276a4df7f82cf9b627221d
aniket2499 <aniketpadsala@gmail.com>
2022-12-04 01:54:08 -0500

    updates

--- client/src/components/SignOut.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import {doSignOut} from '../firebase/FirebaseFunctions';
-import "../App.css";
-
-const SignOutBtn = () => {
-  return <button type="button" onClick={doSignOut}> Sign Out </button>
-}
-
-export default SignOutBtn;
