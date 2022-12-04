d10fa8752e3d8af1910b7a35aca6c79b969f8b8f
aniket2499 <aniketpadsala@gmail.com>
2022-12-04 11:12:47 -0500

    update

--- server/data/flight.js
+++ server/data/flight.js
@@ -1,9 +1,6 @@
 const Amadeus = require("amadeus");
 const path = require("path");
-require("dotenv").config({
-  path: path.resolve(__dirname, "../.env"),
-});
-
+require("dotenv").config({ path: path.resolve(__dirname, "../server/.env") });
 const amadeus = new Amadeus({
   clientId: process.env.API_FLIGHT_KEY,
   clientSecret: process.env.API_FLIGHT_SECRET,
@@ -14,7 +11,7 @@ const getAllFlights = async (Parameters) => {
     const arrivalFlights = await amadeus.shopping.flightOffersSearch.get({
       originLocationCode: Parameters.origin,
       destinationLocationCode: Parameters.dest,
-      departureDate: Parameters.date,
+      departureDate: "2023-06-01",
       adults: Parameters.adults,
       max: "40",
     });
@@ -41,7 +38,7 @@ const getAllFlights = async (Parameters) => {
     const departureFlights = await amadeus.shopping.flightOffersSearch.get({
       originLocationCode: Parameters.origin,
       destinationLocationCode: Parameters.dest,
-      departureDate: Parameters.date,
+      departureDate: "2023-06-01",
       adults: Parameters.adults,
       max: "40",
     });
@@ -75,6 +72,22 @@ const getAllFlights = async (Parameters) => {
   }
 };
 
+const getAllCities = async (city) => {
+  try {
+    console.log(process.env.API_FLIGHT_KEY);
+
+    // const cities = await amadeus.referenceData.locations.get({
+    //   keyword: city,
+    //   subType: ["CITY"],
+    // });
+    console.log(cities);
+    res.status(200).json(cities.data);
+  } catch (err) {
+    throw err;
+  }
+};
+
 module.exports = {
   getAllFlights,
+  getAllCities,
 };
