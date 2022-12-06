af3a22b8aaf93933d004e9ac1b51887166b12431
Mav-ninerniner <mpate138@stevens.edu>
2022-12-06 18:15:17 -0500

    fixed redux functions

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,6 +1,12 @@
 import React from "react";
+import {useSelector, useDispatch} from 'react-redux';
 
 function Home() {
+
+  const allRestaurants = useSelector((state)=>state.restaurants);
+  console.log(allRestaurants)
+
+
   return <div>Home</div>;
 }
 
