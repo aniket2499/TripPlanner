188d9ca766772204c9a35331809f845b81a65163
neeltejani <neeltejani125@gmail.com>
2022-12-07 10:29:57 -0500

    navbar added

--- client/src/reducers/hotelReducer.js
+++ client/src/reducers/hotelReducer.js
@@ -1,28 +1,43 @@
-const initialState = [{
+const initialState = [
+  {
     location_id: null,
     name: null,
     imageUrl: null,
     rating: null,
     latitude: null,
     longitude: null,
-}]
+  },
+];
 
 let copyState = null;
 
-const hotelReducer = (state = initialState, action) =>{
-    const {type, payload} = action;
+const hotelReducer = (state = initialState, action) => {
+  const { type, payload } = action;
 
-    switch(type){
-        case 'ADD_HOTEL':
-            return [...state, {location_id:payload.location_id, name: payload.name, imageUrl: payload.imageUrl, rating: payload.rating, latitude: payload.latitude, longitude: payload.longitude}];
-        
-        case 'DELETE_HOTEL':
-            copyState = [...state];
-            copyState = copyState.filter((x)=> x.location_id!==payload.location_id);
-            return [...copyState];
+  switch (type) {
+    case "ADD_HOTEL":
+      return [
+        ...state,
+        {
+          location_id: payload.location_id,
+          name: payload.name,
+          imageUrl: payload.imageUrl,
+          rating: payload.rating,
+          latitude: payload.latitude,
+          longitude: payload.longitude,
+        },
+      ];
 
-        default: return state;
-    }
-}
+    case "DELETE_HOTEL":
+      copyState = [...state];
+      copyState = copyState.filter(
+        (x) => x.location_id !== payload.location_id,
+      );
+      return [...copyState];
+
+    default:
+      return state;
+  }
+};
 
 export default hotelReducer;
