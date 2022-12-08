b81f12f799d75a19b0229e9e1726786a068d459d
Utsav Italiya <uitaliya@stevens.edu>
2022-12-08 16:05:12 -0500

    login form added, navbar bugs fixed

--- client/src/Components/SocialSignIn.js
+++ client/src/Components/SocialSignIn.js
@@ -1,22 +1,28 @@
-import React from 'react';
-import { doSocialSignIn } from '../firebase/FirebaseFunctions';
+import { Button } from "@mui/material";
+import React from "react";
+import { doSocialSignIn } from "../firebase/FirebaseFunctions";
+import GoogleIcon from "@mui/icons-material/Google";
 
 const SocialSignIn = () => {
-
-    const handleSocialSignIn = async (provider) => {
-        try{
-            await doSocialSignIn(provider);
-        }catch(error){
-            alert(error);
-        }
+  const styles = {
+    button: {
+      marginTop: "1.5rem",
+      borderRadius: "35px",
+    },
+  };
+  const handleSocialSignIn = async (provider) => {
+    try {
+      await doSocialSignIn(provider);
+    } catch (error) {
+      alert(error);
     }
+  };
 
-
-    return(
-        <button>
-            <img src="/imgs/google_signin.png" alt="google sign-in" onClick={() => handleSocialSignIn('google')} />
-        </button>
-    )
+  return (
+    <Button style={styles.button} onClick={() => handleSocialSignIn("google")}>
+      <GoogleIcon />
+    </Button>
+  );
 };
 
 export default SocialSignIn;
